0692d04cd8d1f5a408f939d1a3aa16d0df1c3ef7
Kelson Ameida <kelson.victr@gmail.com>
2024-09-04 19:39:13 -0300

    first commit

---
+++ aula02/src/components/TrabalhandoComImagens.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import VegetaImg from '../assets/vegeta.jpeg'
+
+const TrabalhandoComImagens = () => {
+  return (
+    <div>
+        <img src='goku.jpeg' alt='Goku' />
+        <img src={VegetaImg} alt='Vegeta' />
+    </div>
+  )
+}
+
+export default TrabalhandoComImagens
---
+++ aula1/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Adicao from './components/Adicao'
+
+const App = () => {
+  return (
+    <div>
+      <Adicao num1={2} num2={2} />
+    </div>
+  )
+}
+
+export default App

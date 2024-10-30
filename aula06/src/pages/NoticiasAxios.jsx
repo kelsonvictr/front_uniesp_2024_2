1ea9ab2d2c3116725b06dbb300cf4e9b05b4ab4d
kelsonalmeida <kelson.victr@gmail.com>
2024-10-30 20:52:33 -0300

    aula 06

--- aula06/src/pages/NoticiasAxios.jsx
+++ aula06/src/pages/NoticiasAxios.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect } from 'react'
-import { axios } from 'axios'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 const url = "http://localhost:3000/noticias"
 
